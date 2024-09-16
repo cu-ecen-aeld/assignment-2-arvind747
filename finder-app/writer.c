74e0c68d45a8702b9e37ee5ab7905f35b6b4e2f8
arvind747 <arv747@gmail.com>
2024-09-17 01:38:57 +0530

    syslog added

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <syslog.h>
 
 int main(int argc, char **argv)
 {
@@ -12,10 +13,11 @@ int main(int argc, char **argv)
        printf("incorrect number of argument passed\n");
        return 1;
     }
-
+    openlog("writer", LOG_PERROR|LOG_PID, LOG_USER|LOG_DEBUG);
     fd = open(argv[1], O_WRONLY);
     if(fd == -1)
     {
+        syslog(LOG_ERR, "file does not exist \n");
         printf("file does not exist \n");
         return 1;
     }
@@ -23,15 +25,17 @@ int main(int argc, char **argv)
     nr = write(fd, argv[2], strlen(argv[2]));
     if(nr == -1)
     {
+        syslog(LOG_ERR, "write operation failed \n");
         printf(" write operation failed \n");
         return 1;
     }
     else
     {
+        syslog(LOG_INFO, "write operation passed \n");
+        closelog();
         printf(" write operation passed \n");
         return 0;
     }
-
 
    
 }
